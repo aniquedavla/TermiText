0ac7f5916630e524949952e08c6fa5f52ebf52c2
Anique <aniquedavla@gmail.com>
2016-12-15 15:25:27 -0800

    exploring json parser

--- freeText.sh
+++ freeText.sh
@@ -12,30 +12,35 @@ read text
 echo "Are you ready to send '$text' + to $phoneNumber"
 echo "enter [yes] or [no]"
 read confirm
+
+YES="yes"
+NO="no"
+
+if [ "$confirm" = "$YES" ]; then
+  json = `Curl -X -s POST http://textbelt.com/text -d number=$phoneNumber -d "message=$text" | jq -r '.success'`
+  #echo $json
+  $success=json_decode($json, true)
+  #ArrayOfResponse = json_decode$json
+  echo $success[0]['success']
+  echo $success[0]['message']
+  echo " "
+  echo "Message sent!"
+  exit 0
+else
+  echo "failed to send message, try again"
+  exit 0
+fi
+
 #case $confirm in
 #y|Y Curl -X POST http://textbelt.com/text -d number=$phoneNumber -d "message=$text"
 #echo "Message sent!"
 #exit 0
 #n|N echo "Message not sent, try again!"
 #esac
-YES="yes"
-NO="no"
-if [ "$confirm" = "$YES" ]; then
-#function jsonval {
-#temp=echo $json | sed 's/\\\\\//\//g' | sed 's/[{}]//g' | awk -v k="text" '{n=split($0,a,","); for (i=1; i<=n; i++) print a[i]}' | sed 's/\"\:\"/\|/g' | sed 's/[\,]/ /g' | sed 's/\"//g' | cut -d":" -f2| sed -e 's/^ *//g' -e 's/ *$//g'
-#echo ${temp##*|}
-#}
-json= `Curl -X POST http://textbelt.com/text -d number=$phoneNumber -d "message=$text" | jq -r '.success'`
-echo $json
-success=$json
-echo $success
-echo " "
-echo "Message sent!"
-exit 0
-else 
-echo "failed to send message, try again"
-exit 0
-fi
+##function jsonval {
+  #temp=echo $json | sed 's/\\\\\//\//g' | sed 's/[{}]//g' | awk -v k="text" '{n=split($0,a,","); for (i=1; i<=n; i++) print a[i]}' | sed 's/\"\:\"/\|/g' | sed 's/[\,]/ /g' | sed 's/\"//g' | cut -d":" -f2| sed -e 's/^ *//g' -e 's/ *$//g'
+  #echo ${temp##*|}
+  #}
 
 #
 #
